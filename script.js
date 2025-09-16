44066bc24aa24af519b37d2d2017f07c6ba7cc09
Bright Athuman <brightathuman65@gmail.com>
2025-09-16 12:23:42 +0300

    adding js for canvas in running tracks

--- script.js
+++ script.js
@@ -1,36 +1,63 @@
-function listUp(){
-    var numbers="";
-    var i;
-    for(i=10;i>1;i--){
-        numbers +=" number " + i +"<br>";
-        document.getElementById('demo').innerHTML=numbers;
+// drawing a gradient filled rectangle inside the canva from html  
+var canvas=document.getElementById("myCanvas");
+var ctx=canvas.getContext("2d");
+var grd=ctx.createLinearGradient(2,2,180,300);
+grd.addColorStop(0,"green");
+grd.addColorStop(1,"orange");
+ctx.fillStyle=grd; 
+ctx.fillRect(15,2,180,300);
+ctx.moveTo(0,0);
+ctx.lineTo(180,300);
+ctx.stroke();
+//drawing a circle inside the canva tag from html
+var canvas1=document.getElementById('myCanvas1');
+var ctx=canvas1.getContext('2d');
+ctx.beginPath();
+ctx.arc(125,150,70,90,2*Math.PI);
+ctx.stroke();
+/*testing html input validation using js error "try","catch","finally" and "throw" */
+function myValidation(){
+    var entryData,txt;
+    entryData=document.getElementById('entry').value;
+    txt=document.getElementById('errorText');
+    txt.innerHTML=" ";
+    try{
+        if(entryData=="") throw "can't be empty it must be filled";
+        if(entryData>10) throw "is too large";
+        if(entryData<1) throw "is to small";
+        if(entryData==NaN) throw "is not a number";
     }
-}
-listUp();
-var numbers="";
-var i;
-i=0;
-while(i<20){
-    numbers +="this is number " +i+"<br>";  
-    i++;
-}
-document.getElementById('demo').innerHTML=numbers;
-var numbers="";
-var i;
-i=0;
-do{
-    numbers +="this is number " +i+"<br>";  
-    i++;
-}
-while(i<20){  
-}
-document.getElementById('demo').innerHTML=numbers;
-var numbers="";
-var i;
-for(i=0;i<25;i++){
-    if(i%2==0){
-        continue;
+    catch(err){
+        txt.innerHTML="input " +err;
     }
-    numbers +=+i +"<br>";
+   finally{
+    document.getElementById('entry').value=" "
+   }
 }
-document.getElementById("demo").innerHTML=numbers;
+var car={
+    carName:"VanGuard",
+    carColor:"Black",
+    CarNumber:"T ABC 1234",
+    specs:function(){
+        return this.carName + "  " +this.carColor;
+    }
+};
+document.getElementById('demo').innerHTML=car.specs();
+
+var person1={
+    character:function(){
+        return this.gender + " with "+this.age;
+    }
+};
+   var person2 ={
+    gender:"male",
+    age:23
+   };
+  document.getElementById('demo1').innerHTML= person1.character.call(person2);
+const car={
+    type:"Ford",
+    model:500,
+    color:"white"
+};
+console.log(car);
+                         
