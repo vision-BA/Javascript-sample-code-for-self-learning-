71944821bb7ce4cc1043dec434d2a567009be7ac
bright123-max <brightathuman65@gmail.com>
2026-01-08 13:27:29 +0300

    add TWNstringconcanate.js with string concatenation examples

---
+++ TWNstringconcanate.js
@@ -0,0 +1,27 @@
+var x="123";
+var y="456";
+z=x+y;
+console.log(z); // Output: "123456"
+
+//similar to write
+var a="Hello, ";
+var b="World!";
+var c=a.concat(b);
+console.log(c); // Output: "Hello, World!"
+
+//using template literals
+var firstName="John";
+var lastName="Doe";
+var fullName=`${firstName} ${lastName}`;
+console.log(fullName); // Output: "John Doe"
+
+var a="Hello, ";
+var b="World!";
+var c=`${a}${b}`;
+console.log(c); // Output: "Hello, World!"
+
+var str1="bright";
+var welcomeMessage=`welcome to ${str1} world of IT`;
+console.log(welcomeMessage);
+// Output: "Welcome to bright IT World"
+
